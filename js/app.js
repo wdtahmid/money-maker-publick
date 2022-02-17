775cc7acf5f4671c7a7f3d591e3a0721722449c3
Tahmid Hasan <wdtahmid@gmail.com>
2022-02-17 17:37:52 +0600

    validation added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 
+
 function totalExpenses() {
+
     const message = "Please, input a valid number greater then 0.";
     const expenseMessage = "Please, check your input items, there may be any issue.";
+    const balanceMessage = 'It is not possibale to balance something.';
+    const totalExpenseMessage = 'Your expense is over of your income.';
+
+
+    const saveButton = document.getElementById('save');
 
     let getIncomeInput = parseInt(document.getElementById('input-income').value);
 
@@ -39,23 +46,40 @@ function totalExpenses() {
         document.getElementById('clothes-message').innerText = '';
     }
 
-    console.log(getRentInput);
-    console.log(typeof (getRentInput));
-
 
     const totalExpenses = getFoodInput + getRentInput + getClothesInput;
     const totalBalance = getIncomeInput - totalExpenses;
 
-    if (isNaN(totalBalance) == true) {
-        //document.getElementById('balance-message').innerText = expenseMessage;
-        document.getElementById('balance').innerText = '00';
-    }
-    else {
+
+    if (totalBalance > 0) {
         document.getElementById('balance').innerText = totalBalance;
     }
 
+    else if (totalBalance < 0) {
+        document.getElementById('balance-message').innerText = balanceMessage;
+        document.getElementById('balance').innerText = '00';
+        //saveButton.setAttribute('disabled', true)
+    }
+
+
+    if (totalExpenses > getIncomeInput) {
+        document.getElementById('expense-message').innerText = totalExpenseMessage;
+    }
+
+    else {
+        document.getElementById('expense-message').innerText = '';
+        document.getElementById('balance-message').innerText = '';
+    }
+
+
+
+
+
+    if (isNaN(totalBalance) == true) {
+        document.getElementById('balance').innerText = '00';
+    }
+
     if (isNaN(totalExpenses) == true) {
-        //document.getElementById('balance-message').innerText = expenseMessage;
         document.getElementById('total-expenses').innerText = '00';
     }
     else {
@@ -65,20 +89,49 @@ function totalExpenses() {
     return totalBalance;
 }
 
+function getRemainingBalance(totalBalance) {
+    const getSaveInput = document.getElementById('input-save').value;
+    const saveInputMessage = "It is not possible! keep the % below or equal to 100";
+
+    if (totalBalance > 0) {
+        if (getSaveInput <= 100) {
+
+            const savingAmount = totalBalance / 100 * getSaveInput;
+
+            const remainingBalance = totalBalance - savingAmount;
+
+
+            if (isNaN(savingAmount) == true && isNaN(remainingBalance) == true) {
+                document.getElementById('saving-amount').innerText = '00';
+                document.getElementById('remaining-balance').innerText = '00';
+
+            } else {
+
+                document.getElementById('saving-amount').innerText = savingAmount;
+                document.getElementById('remaining-balance').innerText = remainingBalance;
+            }
+        }
+        else {
+            document.getElementById('save-input-message').innerText = saveInputMessage;
+        }
+    } else {
+        saveButton.setAttribute('disabled', true)
+    }
+
+
+
+
+
+
+}
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
     totalExpenses();
 });
+
 document.getElementById('save').addEventListener('click', function () {
-
     const totalBalance = totalExpenses();
-
-    let getSaveInput = parseInt(document.getElementById('input-save').value);
-
-    const savingAmount = totalBalance / 100 * getSaveInput;
-
-    const remainingBalance = totalBalance - savingAmount;
-
-    document.getElementById('saving-amount').innerText = savingAmount;
-    document.getElementById('remaining-balance').innerText = remainingBalance;
+    const saveButton = totalExpenses();
+    getRemainingBalance(totalBalance, saveButton);
 
 })
